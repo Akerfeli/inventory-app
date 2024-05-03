2352cadc2a4fd6b6a1345fc8770e063837ea9220
Felicia Åkerfelt <felicia.akerfelt@ha.ax>
2024-05-03 11:13:43 +0300

    Update styling

--- src/globalStyles.js
+++ src/globalStyles.js
@@ -1,15 +1,16 @@
 import { StyleSheet } from "react-native";
 
 const Colors = {
-  primaryDark: "1C3346",
+  primaryDark: "#0C2F3B",
   primary: "#256E67",
   secondary: "#AAD3C6",
   tertiary: "#D4F0E4",
   accent: "#F6A497",
   error: "#dc3545",
-  heading: "#021711",
+  heading: "#0C2F3B",
   text: "#0C2F3B",
-  textLight: "#999",
+  textMid: "#999",
+  textLight: "#f8f9fa",
   bgPrimary: "#F5F0E8",
   bgSecondary: "#f8f9fa",
   yellowFlag: "#FFD162",
@@ -20,50 +21,33 @@ const Fonts = {
   bold: "Roboto-Bold",
 };
 
-const FontSizes = {
-  xs: 14,
-  s: 16,
-  m: 20,
-  l: 24,
-};
-
-const Spacing = {
-  xxs: 2,
-  xs: 4,
-  s: 8,
-  m: 16,
-  l: 24,
-  xl: 32,
-  xxl: 40,
-};
-
 const Styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: Colors.bgPrimary,
-    paddingHorizontal: Spacing.m,
-    paddingVertical: Spacing.m,
+    paddingHorizontal: 16,
+    paddingVertical: 16,
   },
   textPrimary: {
     color: Colors.text,
     fontFamily: Fonts.regular,
-    fontSize: FontSizes.s,
+    fontSize: 16,
   },
   textSecondary: {
     color: Colors.textLight,
     fontFamily: Fonts.regular,
-    fontSize: FontSizes.s,
+    fontSize: 14,
   },
   heading: {
     color: Colors.heading,
     fontFamily: Fonts.bold,
-    fontSize: FontSizes.l,
-    marginBottom: Spacing.s,
+    fontSize: 20,
+    marginBottom: 8,
   },
   primaryButton: {
     backgroundColor: Colors.primary,
-    paddingHorizontal: Spacing.m,
-    paddingVertical: Spacing.s,
+    height: 32,
+    width: "100%",
     borderRadius: 16,
     alignItems: "center",
     justifyContent: "center",
@@ -71,8 +55,8 @@ const Styles = StyleSheet.create({
   primaryButtonText: {
     color: "#fff",
     fontFamily: Fonts.bold,
-    fontSize: FontSizes.regular,
+    fontSize: 16,
   },
 });
 
-export { Colors, Fonts, FontSizes, Spacing, Styles };
+export { Colors, Fonts, Styles };
