5bbb4618f77ec5fb5463745dc6124b5dc78391fa
Felicia Åkerfelt <felicia.akerfelt@ha.ax>
2024-05-10 00:42:32 +0300

    Add swipe for delete option

--- src/components/FolderContent.js
+++ src/components/FolderContent.js
@@ -1,11 +1,17 @@
 import { useNavigation } from "@react-navigation/native";
+import { ListItem, Button } from "@rneui/themed";
 import React from "react";
 import { View, FlatList, StyleSheet } from "react-native";
 
 import FolderListItem from "./FolderListItem";
 import ObjectListItem from "./ObjectListItem";
 
-const FolderContent = ({ folderData, folderName }) => {
+const FolderContent = ({
+  folderData,
+  folderName,
+  onDeleteFolder,
+  onDeleteItem,
+}) => {
   const navigation = useNavigation();
 
   const handleNextFolder = (folder) => {
@@ -17,26 +23,63 @@ const FolderContent = ({ folderData, folderName }) => {
     });
   };
 
-  const renderItem = ({ item }) => {
-    if (item.type === "folder") {
-      return (
-        <FolderListItem
-          folderId={item.id}
-          folderName={item.name}
-          onPress={() => handleNextFolder(item)}
-        />
-      );
+  const handleDelete = (itemId, itemType) => {
+    if (itemType === "folder") {
+      onDeleteFolder(itemId);
     } else {
-      // Render as an object
-      return <ObjectListItem item={item} />;
+      onDeleteItem(itemId);
     }
+  };
+
+  const renderSwipeableItem = ({ item }) => {
+    const handlePress = () => {
+      if (item.type === "folder") {
+        handleNextFolder(item);
+      } else {
+        // ToDo: move press item to here from FolderListItem
+      }
+    };
+
+    const handleDeletePress = () => {
+      handleDelete(item.id, item.type);
+    };
+
+    const rightContent = (
+      <Button
+        title="Delete"
+        onPress={handleDeletePress}
+        icon={{ name: "delete", color: "white" }}
+        buttonStyle={{ minHeight: "100%", backgroundColor: "#f86666" }}
+      />
+    );
+
+    return (
+      <ListItem.Swipeable
+        rightContent={rightContent}
+        containerStyle={{ padding: 0 }}
+      >
+        {item.type === "folder" ? (
+          <FolderListItem
+            folderId={item.id}
+            folderName={item.name}
+            onPress={handlePress}
+          />
+        ) : (
+          <ObjectListItem
+            item={item}
+            onPress={handlePress}
+            onDelete={handleDelete}
+          />
+        )}
+      </ListItem.Swipeable>
+    );
   };
 
   return (
     <View style={{ flex: 1, width: "100%" }}>
       <FlatList
         data={folderData}
-        renderItem={renderItem}
+        renderItem={renderSwipeableItem}
         keyExtractor={(item) => item.id}
         ItemSeparatorComponent={() => <View style={styles.separator} />}
       />
