5fe20c7428bb2e54c9fa6b677fcc8f9627184293
Felicia Åkerfelt <felicia.akerfelt@ha.ax>
2024-05-09 15:34:54 +0300

    Move mock data

--- src/components/FolderContent.js
+++ src/components/FolderContent.js
@@ -1,88 +1,41 @@
 import { useNavigation } from "@react-navigation/native";
-import { CheckBox } from "@rneui/themed";
-import React, { useState, useMemo } from "react";
+import React from "react";
 import { View, FlatList, StyleSheet } from "react-native";
 
 import FolderListItem from "./FolderListItem";
 import ObjectListItem from "./ObjectListItem";
 
-const FolderContent = ({ folderId }) => {
+const FolderContent = ({ folderData, folderName }) => {
   const navigation = useNavigation();
-  const [sortByName, setSortByName] = useState(false);
-  const [mockData, setMockData] = useState({
-    id: folderId,
-    name: "Folder 1",
-    objects: [
-      { id: "1", name: "Object a", amount: 1 },
-      { id: "2", name: "Object c", amount: 2 },
-      { id: "3", name: "Object b", amount: 3 },
-    ],
-    subfolders: [
-      { id: "subfolder1", name: "Subfolder 2" },
-      { id: "subfolder2", name: "Subfolder 1" },
-    ],
-  });
 
-  // TODO: Move? Maybe we should let the parent handle the data, and let this component handle the rendering.
-  const sortedContent = useMemo(() => {
-    // Add type field for easier distinction
-    const sortedObjects = mockData.objects.map((item) => ({
-      ...item,
-      type: "object",
-    }));
-    const sortedSubfolders = mockData.subfolders.map((item) => ({
-      ...item,
-      type: "subfolder",
-    }));
-
-    const sortByNameFunction = (a, b) => a.name.localeCompare(b.name);
-
-    if (sortByName) {
-      sortedObjects.sort(sortByNameFunction);
-      sortedSubfolders.sort(sortByNameFunction);
-    }
-
-    return sortedSubfolders.concat(sortedObjects); // Shows folders on top, should we mix them?
-  }, [mockData, sortByName]);
-
-  const handleNextFolder = (folderId) => {
-    const title = mockData.subfolders.find((item) => item.id === folderId).name;
-
+  const handleNextFolder = (folder) => {
     navigation.push("Item Folder", {
-      title,
-      folderId,
-      key: folderId,
-      previousScreenTitle: mockData.name,
+      title: folder.name,
+      folderId: folder.id,
+      key: folder.id,
+      previousScreenTitle: folderName,
     });
   };
 
   const renderItem = ({ item }) => {
-    if (item.type === "object") {
-      return <ObjectListItem item={item} />; //TODO: add the other fields
-    } else {
+    if (item.type === "folder") {
       return (
         <FolderListItem
           folderId={item.id}
           folderName={item.name}
-          onPress={handleNextFolder}
+          onPress={() => handleNextFolder(item)}
         />
       );
+    } else {
+      // Render as an object
+      return <ObjectListItem item={item} />;
     }
   };
 
   return (
     <View style={{ flex: 1, width: "100%" }}>
-      {/*TODO: Move the checkbox to a filter dropdown/modal in the parent */}
-      <CheckBox
-        checked={sortByName}
-        onPress={() => setSortByName(!sortByName)}
-        title="Sort by name"
-        containerStyle={{
-          backgroundColor: "rgba(0,0,0,0)",
-        }}
-      />
       <FlatList
-        data={sortedContent}
+        data={folderData}
         renderItem={renderItem}
         keyExtractor={(item) => item.id}
         ItemSeparatorComponent={() => <View style={styles.separator} />}
