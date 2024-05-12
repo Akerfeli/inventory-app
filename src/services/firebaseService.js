962efc5fda17223e91dd96a2bd9d6b3c63973176
Anna Ö <anna.ostman24@gmail.com>
2024-05-12 10:51:42 +0300

    Refactor logic to one fetchData function, add deleteItem function

--- src/services/firebaseService.js
+++ src/services/firebaseService.js
@@ -4,21 +4,16 @@ import {
   query,
   where,
   collectionGroup,
+  doc,
+  deleteDoc,
+  getDoc,
 } from "firebase/firestore";
 
 import { db } from "../../firebaseConfig";
 
-// ToDo: Refactor the code and reuse some parts
-export const getRootFolderContent = async (uid) => {
+const fetchData = async (q) => {
   try {
-    const q = query(
-      collection(db, "folder-data"),
-      where("createdBy", "==", uid),
-      where("name", "==", "root")
-    );
-
     const querySnapshot = await getDocs(q);
-
     const data = [];
     querySnapshot.forEach((doc) => {
       data.push({
@@ -26,84 +21,30 @@ export const getRootFolderContent = async (uid) => {
         ...doc.data(),
       });
     });
-    console.log("From firebaseService.js ", data);
-
+    // ToDo: comment
+    console.log("Fetched data: ", data);
     return data;
   } catch (error) {
     console.error("Error fetching response:", error);
   }
 };
 
-export const getAllFolders = async (uid) => {
-  try {
-    // Step 1: Retrieve all folders
-    const folderQuery = query(
-      collection(db, "folder-data"),
-      where("createdBy", "==", uid)
-    );
-    const folderQuerySnapshot = await getDocs(folderQuery);
-
-    const folders = [];
-    folderQuerySnapshot.forEach((folderDoc) => {
-      folders.push({
-        id: folderDoc.id,
-        ...folderDoc.data(),
-      });
-    });
-
-    // Step 2: Fetch subfolders for each folder
-    for (const folder of folders) {
-      const subfolderQuery = query(
-        collectionGroup(db, "subfolders"),
-        where("folderId", "==", folder.id)
-      );
-      const subfolderQuerySnapshot = await getDocs(subfolderQuery);
-
-      const subfolders = [];
-      subfolderQuerySnapshot.forEach((subfolderDoc) => {
-        const subfolderData = {
-          id: subfolderDoc.id,
-          ...subfolderDoc.data(),
-        };
-        subfolders.push(subfolderData);
-        // Log subfolder data
-        /* console.log("Subfolder Data:", subfolderData); */
-      });
-
-      folder.subfolders = subfolders;
-
-      // Log folder with subfolders
-      /*  console.log("Folder:", folder); */
-    }
-
-    return folders;
-  } catch (error) {
-    console.error("Error fetching response:", error);
-  }
+/*----------------------- GETS -----------------------*/
+export const getRootFolderContent = async (uid) => {
+  const q = query(
+    collection(db, "folder-data"),
+    where("createdBy", "==", uid),
+    where("name", "==", "root")
+  );
+  return await fetchData(q);
 };
 
 export const getFolderContent = async (documentId) => {
-  try {
-    const q = query(
-      collection(db, "folder-data"),
-      where("__name__", "==", documentId)
-    );
-
-    const querySnapshot = await getDocs(q);
-
-    const data = [];
-    querySnapshot.forEach((doc) => {
-      data.push({
-        id: doc.id,
-        ...doc.data(),
-      });
-    });
-    console.log("From firebaseService.js ", data);
-
-    return data;
-  } catch (error) {
-    console.error("Error fetching response:", error);
-  }
+  const q = query(
+    collection(db, "folder-data"),
+    where("__name__", "==", documentId)
+  );
+  return await fetchData(q);
 };
 
 export const getItemsToBuy = async (uid) => {
@@ -112,18 +53,7 @@ export const getItemsToBuy = async (uid) => {
     where("createdBy", "==", uid),
     where("shoppingList", "==", true)
   );
-  const querySnapshot = await getDocs(q);
-
-  const data = [];
-  querySnapshot.forEach((doc) => {
-    data.push({
-      id: doc.id,
-      ...doc.data(),
-    });
-  });
-  console.log("From firebaseService.js ", data);
-
-  return data;
+  return await fetchData(q);
 };
 
 export const getFavorites = async (uid) => {
@@ -132,16 +62,31 @@ export const getFavorites = async (uid) => {
     where("createdBy", "==", uid),
     where("favoriteList", "==", true)
   );
-  const querySnapshot = await getDocs(q);
+  return await fetchData(q);
+};
 
-  const data = [];
-  querySnapshot.forEach((doc) => {
-    data.push({
-      id: doc.id,
-      ...doc.data(),
-    });
-  });
-  console.log("From firebaseService.js ", data);
+export const getAllFolders = async (uid) => {
+  // Step 1: Retrieve all folders
+  const folderQuery = query(
+    collection(db, "folder-data"),
+    where("createdBy", "==", uid)
+  );
+  const folders = await fetchData(folderQuery);
 
-  return data;
+  return folders;
+};
+
+/*----------------------- DELETE -----------------------*/
+
+export const deleteItem = async (collectionPath, documentId) => {
+  // Check if the document exists
+  const itemDoc = doc(db, collectionPath, documentId);
+  const itemSnapshot = await getDoc(itemDoc);
+
+  if (itemSnapshot.exists()) {
+    await deleteDoc(itemDoc);
+    console.log("Document deleted successfully with id: ", documentId);
+  } else {
+    console.log("Document does not exist with id: ", documentId);
+  }
 };
