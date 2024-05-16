42e0d9886da8245aa30b1ffad6738723823bc3c5
Felicia Åkerfelt <felicia.akerfelt@ha.ax>
2024-05-16 23:35:15 +0300

    Fix real time data

--- src/screens/ItemFolderScreen.js
+++ src/screens/ItemFolderScreen.js
@@ -1,29 +1,51 @@
 import { useRoute } from "@react-navigation/native";
-import React, { useState } from "react";
+import React, { useState, useMemo } from "react";
 import { View, ActivityIndicator, StyleSheet } from "react-native";
 
 import Breadcrumbs from "../components/Breadcrumbs";
 import FolderContent from "../components/FolderContent";
 import FolderCreationModal from "../components/FolderCreationModal";
 import FolderMenu from "../components/FolderMenu";
+import { useAuth } from "../contexts/AuthContext";
 import { Colors } from "../globalStyles";
 import useFetch from "../hooks/useFetch";
 import useFlattenFolderContent from "../hooks/useFlattenFolderContent";
 import { getFolderContentById } from "../services/firebaseService";
+import { getItems, getSubFolders } from "../services/firebaseNewService";
+import useNewFetch from "../hooks/useNewFetch";
 
 const ItemFolderScreen = () => {
   const route = useRoute();
   const { previousScreenTitle, title, folderId } = route.params;
   const [modalVisible, setModalVisible] = useState(false);
 
+  const { userState } = useAuth();
+
+  const params = [folderId];
   const {
-    data: folderData,
-    isLoading,
-    error,
-  } = useFetch({
-    firebaseFunction: () => getFolderContentById(folderId),
-  });
-  const flatContent = useFlattenFolderContent(folderData);
+    data: dataItems,
+    isLoading: isLoadingItems,
+    error: errorItems,
+  } = useNewFetch(getItems, params);
+  const {
+    data: dataSubfolders,
+    isLoading: isLoadingSubfolders,
+    error: errorSubfolders,
+  } = useNewFetch(getSubFolders, params);
+
+  const isLoading = isLoadingItems || isLoadingSubfolders;
+
+  const flatContent = useMemo(() => {
+    if (isLoading || !dataSubfolders || !dataItems) {
+      return null;
+    }
+
+    const flatArray = dataSubfolders
+      .map((subfolder) => ({ ...subfolder, type: "folder" }))
+      .concat(dataItems.map((obj) => ({ ...obj, type: "object" })));
+
+    return flatArray;
+  }, [dataItems, dataSubfolders, isLoading]);
 
   return (
     <View
